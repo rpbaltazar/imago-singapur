54ffaee315e89ad10ae8856b23a52c86fe932a24
Rui Baltazar <rui.p.baltazar@gmail.com>
2014-09-03 20:38:22 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SingaporeStories::Application.config.secret_key_base = 'f72f2305f817ec9e95b60cddf15d8fbed2633c690c358964e80064cf68f84ca9600c5a53a0a236a6726476b86a8385a30f85883893e4f25c58080effe53a938a'
